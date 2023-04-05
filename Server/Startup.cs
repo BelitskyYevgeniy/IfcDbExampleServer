360d4bc4a11d625722211edbf656c4c9dc8448c0
BelitskyYevgeniy <78808088+BelitskyYevgeniy@users.noreply.github.com>
2023-04-05 23:36:47 +0300

    setup cors

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -30,6 +30,8 @@ namespace Server
             services.AddDataHelpers();
 
             services.RegisterServices();
+
+            services.AddCors();
             
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -51,6 +53,12 @@ namespace Server
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
             app.UseErrorHandling();
 
